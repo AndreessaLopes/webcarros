b2305a7b32000d63ebc69a701e94a1fd07cbdb4b
Andressa <andressacaroline082011@gmail.com>
2025-04-10 15:20:54 -0300

    feat: adiciona componente Private para proteção de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home } from './pages/home';
-import { CarDetail } from './pages/car';
-import { Dashboard } from './pages/dashboard';
-import { Register } from './pages/register';
-import { Login } from './pages/login';
-import { New } from './pages/dashboard/new';
-import { Layout } from './components/layout';
+import { Home } from "./pages/home";
+import { CarDetail } from "./pages/car";
+import { Dashboard } from "./pages/dashboard";
+import { Register } from "./pages/register";
+import { Login } from "./pages/login";
+import { New } from "./pages/dashboard/new";
+import { Layout } from "./components/layout";
+import { Private } from "./routes/Private";
 
 const router = createBrowserRouter([
   {
@@ -21,11 +22,19 @@ const router = createBrowserRouter([
       },
       {
         path: "/dashboard",
-        element: <Dashboard />,
+        element: (
+          <Private>
+            <Dashboard />
+          </Private>
+        ),
       },
       {
         path: "/dashboard/new",
-        element: <New />,
+        element: (
+          <Private>
+            <New />
+          </Private>
+        ),
       },
     ],
   },
