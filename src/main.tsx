5e8c0df925a3bcad1ae32ffd91d53fca35bc8ed3
Andressa <andressacaroline082011@gmail.com>
2025-04-16 13:12:03 -0300

    feat: adiciona carrossel de imagens e detalhes do carro na página de detalhes

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { router } from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { router } from "./App";
+import "./index.css";
 
-import { RouterProvider } from 'react-router-dom'
-import AuthProvider from './contexts/AuthContext'
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./contexts/AuthContext";
 
+import { register } from "swiper/element/bundle";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+register();
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
